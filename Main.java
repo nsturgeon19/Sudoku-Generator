7ffd2f8f5ac5a6cf18508f4dbdb5655b55adfb64
nsturgeon19 <nsturgeon19@dtechhs.org>
2019-05-07 09:44:42 -0700

    Commenting and spelling error related things.

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ class Main {
     System.out.println("Hello world!");
 
     // Or shift an existing puzzle.
-    int[][] solvedSudokuPuzle = {
+    int[][] solvedSudokuPuzzle = {
       {1,2,3,4,5,6,7,8,9},
       {7,8,9,1,2,3,4,5,6},
       {4,5,6,7,8,9,1,2,3},
@@ -21,57 +21,58 @@ class Main {
     Random rand = new Random();
 
     // Shuffles the rows, one box at a time.
-    for(int box = 0; box < solvedSudokuPuzle.length; box += 3){
+    for(int box = 0; box < solvedSudokuPuzzle.length; box += 3){
       // Picks a boxes' worth of rows.
-      int[][] rowsInBox = {solvedSudokuPuzle[box],solvedSudokuPuzle[box+1],solvedSudokuPuzle[box+2]};
+      int[][] rowsInBox = {solvedSudokuPuzzle[box],solvedSudokuPuzzle[box+1],solvedSudokuPuzzle[box+2]};
       rowsInBox = genPuzzleTools.shuffleRows(rowsInBox);
 
       // gives the original puzzle the shuffled rows.
       for (int row = 0; row < 3; row++){
-        solvedSudokuPuzle[box+row] = rowsInBox[row];
+        solvedSudokuPuzzle[box+row] = rowsInBox[row];
       }
     }
 
     // prints the array after row shuffle
-    // printArray(solvedSudokuPuzle);
+//     printArray(solvedSudokuPuzzle);
     System.out.println();
 
-    for(int box = 0; box < solvedSudokuPuzle.length; box += 3) {
+    for(int box = 0; box < solvedSudokuPuzzle.length; box += 3) {
       // Creates a representation of the columns that need to be shuffled.
       int[][] columnsToShuffle = new int[9][3];
-      for (int row = 0; row < solvedSudokuPuzle.length; row++) {
+      for (int row = 0; row < solvedSudokuPuzzle.length; row++) {
         for (int column = 0; column < 3; column++) {
-          columnsToShuffle[row][column] = solvedSudokuPuzle[row][box+column];
+          columnsToShuffle[row][column] = solvedSudokuPuzzle[row][box+column];
         }
       }
 
       columnsToShuffle = genPuzzleTools.shuffleColumns(columnsToShuffle);
 
-      for (int row = 0; row < solvedSudokuPuzle.length; row++) {
+      for (int row = 0; row < solvedSudokuPuzzle.length; row++) {
         for (int column = 0; column < 3; column++) {
-          solvedSudokuPuzle[row][box+column] = columnsToShuffle[row][column];
+          solvedSudokuPuzzle[row][box+column] = columnsToShuffle[row][column];
         }
       }
     }
 
     // Print after column shuffle.
-    // printArray(solvedSudokuPuzle);
+    // printArray(solvedSudokuPuzzle);
     System.out.println();
 
+    // shuffles the numbers in the puzzle
     int[] numShuffleKey = genPuzzleTools.fisherYatesShuffle();
-    for (int row = 0; row < solvedSudokuPuzle.length; row++){
-      for (int column = 0; column < solvedSudokuPuzle[0].length; column++){
-        solvedSudokuPuzle[row][column] = numShuffleKey[solvedSudokuPuzle[row][column]-1];
+    for (int row = 0; row < solvedSudokuPuzzle.length; row++){
+      for (int column = 0; column < solvedSudokuPuzzle[0].length; column++){
+        solvedSudokuPuzzle[row][column] = numShuffleKey[solvedSudokuPuzzle[row][column]-1];
       }
     }
 
     // Print final puzzle
-    printArray(solvedSudokuPuzle);
+    printArray(solvedSudokuPuzzle);
     System.out.println();
-    printArrayRaw(solvedSudokuPuzle);
+    printArrayRaw(solvedSudokuPuzzle);
     System.out.println();
 
-    int[][] notSolvedSudokuPuzzle = solvedSudokuPuzle;
+    int[][] notSolvedSudokuPuzzle = solvedSudokuPuzzle;
     // Generates a random num of emptyCells from 40 to 60.
     int emptyCells = rand.nextInt(21) + 40;
     for (int removeNum = 0; removeNum < emptyCells; removeNum++){
