c4b6f87e6bb855403b38fa6dbb3e002f367b6d4e
nsturgeon19 <nsturgeon19@dtechhs.org>
2019-05-07 17:47:47 -0700

    Main now adds candidates to the array.

--- Main.java
+++ Main.java
@@ -13,10 +13,15 @@ class Main {
     // Adds a third dimension for candidates in the puzzle.
     for (int row = 0; row < candidatePuzzle.length; row++) {
       for (int column = 0; column < candidatePuzzle[0].length; column++){
-        if (basicPuzzle[row][column] != 0){
-          candidatePuzzle[row][column][0] = basicPuzzle[row][column];
-        } else {
-          // TODO write a method that looks for the possible candidates.
+        candidatePuzzle[row][column][0] = basicPuzzle[row][column];
+      }
+    }
+
+    // Adds the candidates to the puzzle
+    for (int row = 0; row < candidatePuzzle.length; row++) {
+      for (int column = 0; column < candidatePuzzle[0].length; column++) {
+        if (candidatePuzzle[row][column][0] == 0) {
+          candidatePuzzle[row][column] = makeCandidates(candidatePuzzle, row, column);
         }
       }
     }
@@ -39,6 +44,29 @@ class Main {
         System.out.print(puzzle[row][column]);
       }
     }
+  }
+
+  // Checks for any easy eliminations before putting the possibilities in.
+  public static int[] makeCandidates (int[][][] candidatePuzzle, int row, int column) {
+    int[] candidatePoss = {1,2,3,4,5,6,7,8,9};
+
+    for (int currColumn = 0; currColumn < candidatePuzzle.length; currColumn++){
+      if (candidatePuzzle[row][currColumn][0] != 0 && candidatePuzzle[row][currColumn][1] == 0){
+        candidatePoss[candidatePuzzle[row][currColumn][0]-1] = 0;
+      }
+    }
+
+    for (int currRow = 0; currRow < candidatePuzzle[0].length; currRow++){
+      if (candidatePuzzle[currRow][column][0] != 0 && candidatePuzzle[currRow][column][1] == 0){
+        candidatePoss[candidatePuzzle[currRow][column][0]-1] = 0;
+      }
+    }
+
+    // TODO add box elims.
+
+    // TODO shuffle candidates forward so all 0's are at the back.
+
+    return candidatePoss;
   }
 
 }
